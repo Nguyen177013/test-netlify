c9221ed465fb7b5d3be2bff66bfa0cdc8d200d14
Nguyen177013 <phannguyendoremon@gmail.com>
2023-03-21 18:49:57 +0700

    change Vans component logic

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,41 +1,56 @@
 import "../../assets/styles/Vans.css";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import VanItems from "../Vans/VanItem";
-const Vans = ({url}) => {
+const Vans = ({ url }) => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const typeFilter = searchParams.get("type");
     const [vans, setVans] = useState(null);
     const [pending, setPending] = useState(true);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(url)
-        .then(res=>{
-            if(!res.ok){
-                throw Error("Server can not fetch the url please try again!");
-            }
-            return res.json();
-        })
-        .then(data=>{
-            setVans(data);
-            setPending(false);
-        })
-    },[url])
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Server can not fetch the url please try again!");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setVans(data);
+                setPending(false);
+            })
+    }, [url])
+    const displayVan = (typeFilter && vans) ? vans.filter(van => van.type === typeFilter) : vans;
     return (
         <div className="container">
             <div className="vans__container">
                 <h1>Explore our van options</h1>
                 <div className="vans__options">
-                    <button className="option__btn btn">Simple</button>
-                    <button className="option__btn btn">Luxury</button>
-                    <button className="option__btn btn">Rugged</button>
-                    <p className="option__clear">Clear filters</p>
+                    <button
+                        className="option__btn btn"
+                        onClick={() => setSearchParams({ type: "simple" })}
+                    >Simple</button>
+                    <button
+                        className="option__btn btn"
+                        onClick={() => setSearchParams({ type: "luxury" })}
+                    >Luxury</button>
+                    <button
+                        className="option__btn btn"
+                        onClick={() => setSearchParams({ type: "rugged" })}
+                    >Rugged</button>
+                    <p
+                        className="option__clear"
+                        onClick={() => setSearchParams()}
+                    >Clear filters</p>
                 </div>
                 <div className="list__vans">
                     {pending && <p>Please Wait for the server...</p>}
-                    {vans && vans.map(van=>(
-                    <Link to={`/vans/${van.id}` } key = {van.id} className="van">
-                    <VanItems {...van}></VanItems>
-                    </Link>
+                    {vans && displayVan.map(van => (
+                        <Link to={`/vans/${van.id}`} key={van.id} className="van">
+                            <VanItems {...van}></VanItems>
+                        </Link>
                     ))
-                }
+                    }
                 </div>
             </div>
         </div>
